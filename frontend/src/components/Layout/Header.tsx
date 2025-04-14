e4a084e289bae2601d35c8bc3e2f6335d4c4a32b
Raina Moon <mds6425@gmail.com>
2025-04-14 18:02:33 +0900

    ✨ feat: implement language toggle functionality in Header component

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useLanguageStore } from "@/libs/languageStore";
+import React from "react";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const { lang, setLang } = useLanguageStore();
 
-export default Header
+  const toggleLanguage = () => {
+    setLang(lang === "en" ? "ko" : "en");
+  };
+
+  return <div>Header</div>;
+};
+
+export default Header;
