1fe9fcd781d50385452b76a37d95e804adbaf98a
Raina Moon <mds6425@gmail.com>
2025-04-14 17:04:26 +0900

    ✅ test: update page component to display a styled heading with Tailwind CSS

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="text-[#eae]">page</div>;
+  return <h1 className="text-4xl font-bold text-red-500">Hello, Tailwind!</h1>;
 };
 
 export default page;
