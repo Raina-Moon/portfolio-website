5d256aaf8062ab7ccaf505910d9823a48818851c
Raina Moon <mds6425@gmail.com>
2025-04-14 15:41:10 +0900

    ✨ feat: refactor globals.css to use Tailwind CSS preflight and utilities

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  return <div className="text-[#eae]">page</div>;
+};
 
-export default page
+export default page;
