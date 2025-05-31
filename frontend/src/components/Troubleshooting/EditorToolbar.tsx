fcbdc1a245fec3393c9d0b760847d96c3b151829
Raina Moon <mds6425@gmail.com>
2025-05-31 18:47:13 +0900

    ✨ feat: enhance EditorToolbar with text alignment options and remove unused buttons

--- frontend/src/components/Troubleshooting/EditorToolbar.tsx
+++ frontend/src/components/Troubleshooting/EditorToolbar.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import { Editor } from "@tiptap/react";
+import {
+  FaAlignCenter,
+  FaAlignLeft,
+  FaAlignRight,
+  FaBold,
+  FaCode,
+  FaHighlighter,
+  FaImage,
+  FaLink,
+} from "react-icons/fa";
+import { FaListCheck, FaSquarePollHorizontal } from "react-icons/fa6";
+import { MdFormatListNumbered } from "react-icons/md";
 
 const EditorToolbar = ({
   editor,
@@ -13,83 +25,38 @@ const EditorToolbar = ({
   return (
     <div className="control-group">
       <div className="button-group">
-        <button
-          onClick={() => editor.chain().focus().setBlockquote().run()}
-          disabled={!editor.can().setBlockquote()}
-        >
-          Set blockquote
-        </button>
-        <button
-          onClick={() => editor.chain().focus().unsetBlockquote().run()}
-          disabled={!editor.can().unsetBlockquote()}
-        >
-          Unset blockquote
-        </button>
+        
         <button
           onClick={() => editor.chain().focus().toggleCodeBlock().run()}
           className={editor.isActive("codeBlock") ? "is-active" : ""}
         >
-          Toggle code block
+          <FaCode/>
         </button>
         <button
           onClick={() => editor.chain().focus().toggleTaskList().run()}
           className={editor.isActive("taskList") ? "is-active" : ""}
         >
-          Toggle task list
-        </button>
-        <button
-          onClick={() => editor.chain().focus().splitListItem("taskItem").run()}
-          disabled={!editor.can().splitListItem("taskItem")}
-        >
-          Split list item
-        </button>
-        <button
-          onClick={() => editor.chain().focus().sinkListItem("taskItem").run()}
-          disabled={!editor.can().sinkListItem("taskItem")}
-        >
-          Sink list item
-        </button>
-        <button
-          onClick={() => editor.chain().focus().liftListItem("taskItem").run()}
-          disabled={!editor.can().liftListItem("taskItem")}
-        >
-          Lift list item
+          <FaListCheck/>
         </button>
         <button
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
           className={editor.isActive("orderedList") ? "is-active" : ""}
         >
-          Toggle ordered list
-        </button>
-        <button
-          onClick={() => editor.chain().focus().splitListItem("listItem").run()}
-          disabled={!editor.can().splitListItem("listItem")}
-        >
-          Split list item
-        </button>
-        <button
-          onClick={() => editor.chain().focus().sinkListItem("listItem").run()}
-          disabled={!editor.can().sinkListItem("listItem")}
-        >
-          Sink list item
-        </button>
-        <button
-          onClick={() => editor.chain().focus().liftListItem("listItem").run()}
-          disabled={!editor.can().liftListItem("listItem")}
-        >
-          Lift list item
+          <MdFormatListNumbered />
         </button>
         <button
           onClick={() => editor.chain().focus().setHorizontalRule().run()}
         >
-          Set horizontal rule
+          <FaSquarePollHorizontal />
         </button>
-        <button onClick={addImage}>Set image</button>
+        <button onClick={addImage}>
+          <FaImage />
+        </button>
         <button
           onClick={() => editor.chain().focus().toggleBold().run()}
           className={editor.isActive("bold") ? "is-active" : ""}
         >
-          Toggle bold
+          <FaBold />
         </button>
         <button
           onClick={() =>
@@ -101,25 +68,20 @@ const EditorToolbar = ({
               : ""
           }
         >
-          Blue
+          <FaHighlighter />
         </button>
         <button
           onClick={setLink}
           className={editor.isActive("link") ? "is-active" : ""}
         >
-          Set link
+          <FaLink />
         </button>
-        <button
-          onClick={() => editor.chain().focus().unsetLink().run()}
-          disabled={!editor.isActive("link")}
-        >
-          Unset link
-        </button>
+
         <button
           onClick={() => editor.chain().focus().setTextAlign("left").run()}
           className={editor.isActive({ textAlign: "left" }) ? "is-active" : ""}
         >
-          Left
+          <FaAlignLeft />
         </button>
         <button
           onClick={() => editor.chain().focus().setTextAlign("center").run()}
@@ -127,22 +89,15 @@ const EditorToolbar = ({
             editor.isActive({ textAlign: "center" }) ? "is-active" : ""
           }
         >
-          Center
+          <FaAlignCenter />
         </button>
         <button
           onClick={() => editor.chain().focus().setTextAlign("right").run()}
           className={editor.isActive({ textAlign: "right" }) ? "is-active" : ""}
         >
-          Right
+          <FaAlignRight />
         </button>
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("justify").run()}
-          className={
-            editor.isActive({ textAlign: "justify" }) ? "is-active" : ""
-          }
-        >
-          Justify
-        </button>
+
         <input
           type="color"
           onInput={(event) =>
@@ -151,7 +106,6 @@ const EditorToolbar = ({
           value={editor.getAttributes("textStyle").color}
           data-testid="setColor"
         />
-
       </div>
     </div>
   );
