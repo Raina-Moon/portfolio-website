dfd128d7b213155bc5629f8c9c1d0ce0d346303b
Raina Moon <mds6425@gmail.com>
2025-05-31 15:46:24 +0900

    ✨ feat: implement TinyMCE editor in TroubleshootingSection component

--- frontend/src/components/Troubleshooting/TroubleshootingSection.tsx
+++ frontend/src/components/Troubleshooting/TroubleshootingSection.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+"use client"
+
+import { Editor } from '@tinymce/tinymce-react'
+import React, { useRef } from 'react'
 
 const TroubleshootingSection = () => {
+  const editorRef = useRef(null)
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current)
+    } else {
+      console.error('Editor is not initialized')
+    }
+  }
+  
   return (
-    <div>TroubleshootingSection</div>
+    <div><>
+      <Editor
+        apiKey='no-api-key'
+        onInit={ (_evt, editor) => editorRef.current = editor }
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+          ],
+          toolbar: 'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </></div>
   )
 }
 
