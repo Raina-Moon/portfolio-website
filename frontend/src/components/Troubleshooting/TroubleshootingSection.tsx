772329c1cdcbfa2b167def47bf09b52c82c47365
Raina Moon <mds6425@gmail.com>
2025-06-02 03:04:00 +0900

    ✨ feat: update addImage function to handle file uploads and integrate with Cloudinary

--- frontend/src/components/Troubleshooting/TroubleshootingSection.tsx
+++ frontend/src/components/Troubleshooting/TroubleshootingSection.tsx
@@ -5,23 +5,37 @@ import { EditorContent } from "@tiptap/react";
 import EditorToolbar from "./EditorToolbar";
 import { useTiptapEditor } from "@/hooks/useTiptapEditor";
 import { createTroubleshootingPost } from "@/libs/api/troubleshooting";
+import { uploadImageToCloudinary } from "@/hooks/uploadImageToCloudinary";
 
 const TroubleshootingSection = () => {
   const editor = useTiptapEditor();
 
-  const addImage = useCallback(() => {
-    const url = window.prompt("URL");
+  const addImage = async (file: File) => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
 
-    if (editor && url) {
-      editor.chain().focus().setImage({ src: url }).run();
-    }
-  }, [editor]);
+    input.onchange = async () => {
+      const files = input.files?.[0];
+      if (!files) return;
 
-  const saveContent = async() => {
+      try {
+        const imageUrl = await uploadImageToCloudinary(files);
+        editor?.chain().focus().setImage({ src: imageUrl }).run();
+      } catch (err) {
+        console.error("Failed to upload image:", err);
+        alert("Failed to upload image. Please try again.");
+      }
+    };
+
+    input.click();
+  };
+
+  const saveContent = async () => {
     const html = editor?.getHTML() ?? "";
 
     await createTroubleshootingPost(html);
-  }
+  };
 
   const setLink = useCallback(() => {
     if (!editor) {
@@ -61,7 +75,7 @@ const TroubleshootingSection = () => {
 
   return (
     <div>
-      <EditorToolbar editor={editor} addImage={addImage} setLink={setLink} />
+      <EditorToolbar editor={editor} addImage={() => addImage} setLink={setLink} />
 
       <EditorContent editor={editor} />
       <button onClick={saveContent}>Save</button>
