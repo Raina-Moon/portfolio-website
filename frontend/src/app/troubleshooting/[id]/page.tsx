52a3b7f22d58b43162ecc2db68d379ff7b766b23
Raina Moon <mds6425@gmail.com>
2025-06-02 19:41:52 +0900

    ♻️ refactor: improve layout and styling of troubleshooting post page

--- frontend/src/app/troubleshooting/[id]/page.tsx
+++ frontend/src/app/troubleshooting/[id]/page.tsx
@@ -6,9 +6,14 @@ const page = async ({ params }: { params: { id: string } }) => {
   const post = await fetchTroubleshootingPost(id);
 
   return (
-    <div>
-      <p>{post.title}</p>
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
+    <div className="flex flex-col p-4">
+      <div className="flex justify-between items-center mb-4 mx-4">
+      <p className="text-4xl font-semibold">{post.title}</p>
+      <p className="text-grey-700 text-sm mt-6">{new Date(post.createdAt).toDateString()}</p>
+      </div>
+      <div className="border mx-4"/>
+      <div dangerouslySetInnerHTML={{ __html: post.content }} 
+      className="mt-7 mb-20"/>
     </div>
   );
 };
