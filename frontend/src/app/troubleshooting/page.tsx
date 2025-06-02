fe9718e37e3b62573df0f5bf0e5ffee1b4993149
Raina Moon <mds6425@gmail.com>
2025-06-02 14:41:57 +0900

    ✨ feat: implement fetchPosts to retrieve and display troubleshooting posts

--- frontend/src/app/troubleshooting/page.tsx
+++ frontend/src/app/troubleshooting/page.tsx
@@ -1,19 +1,40 @@
-"use client"
+"use client";
 
 import { fetchTroubleshootingPosts } from "@/libs/api/troubleshooting";
-import React, { useState } from "react";
+import { Troubleshooting } from "@/libs/types";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
-  const [posts,setPosts] = useState([])
-  const fetchData = fetchTroubleshootingPosts();
-  return (
-  <div>
-    {posts.map((item,idx) => (
-      <li>
+  const [posts, setPosts] = useState<Troubleshooting[]>([]);
 
-      </li>
-    ))}
-    </div>);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const data = await fetchTroubleshootingPosts();
+        setPosts(data);
+      } catch (err) {
+        console.error("Failed to fetch troubleshooting posts:", err);
+        alert("Failed to fetch troubleshooting posts. Please try again later.");
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {posts.map((item) => (
+          <li key={item.id}>
+            <Link href={`/troubleshooting/${item.id}`}>
+              <p>{item.title}</p>
+              <p>{item.createdAt}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default page;
