f52b0fe314226c3554c5960f5945d78686ef1317
Raina Moon <mds6425@gmail.com>
2025-06-03 02:21:12 +0900

    🚑fix : refactor Page component to use useEffect for data fetching and improve loading state handling

--- frontend/src/app/troubleshooting/[id]/page.tsx
+++ frontend/src/app/troubleshooting/[id]/page.tsx
@@ -1,15 +1,41 @@
-import { fetchTroubleshootingPost } from "@/libs/api/troubleshooting";
-import React from "react";
+"use client"
 
-const Page = async ({ params }: { params: { id: string } }) => {
-  const id = parseInt(params.id, 10);
-  const post = await fetchTroubleshootingPost(id);
+import { fetchTroubleshootingPost } from "@/libs/api/troubleshooting";
+import { Troubleshooting } from "@/types/types";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const Page = () => {
+  const params = useParams()
+  const [post, setPost] = useState<Troubleshooting | null>(null);
+
+  useEffect(() => {
+    if (!params?.id) return;
+
+    const fetchData = async () => {
+      try {
+        const data = await fetchTroubleshootingPost(Number(params.id));
+        setPost(data);
+      } catch (err) {
+        console.error("Error fetching post:", err);
+      }
+    };
+    fetchData();
+},[params])
+
+  if (!post) {
+    return (
+      <div className="flex flex-col p-4">
+        <p>Loading...</p>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col p-4">
       <div className="flex justify-between items-center mb-4 mx-4">
-      <p className="text-4xl font-semibold">{post.title}</p>
-      <p className="text-grey-700 text-sm mt-6">{new Date(post.createdAt).toDateString()}</p>
+        <p className="text-4xl font-semibold">{post.title}</p>
+        <p className="text-grey-700 text-sm mt-6">{new Date(post.createdAt).toDateString()}</p>
       </div>
       <div className="border mx-4"/>
       <div dangerouslySetInnerHTML={{ __html: post.content }} 
