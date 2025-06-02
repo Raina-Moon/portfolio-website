e1a51b5b00abdd5be9ecb4faf5cb10c39718e766
Raina Moon <mds6425@gmail.com>
2025-06-02 17:18:19 +0900

    ♻️refactor : refactor troubleshooting post page to include delete functionality and improve state management

--- frontend/src/app/troubleshooting/edit/[id]/page.tsx
+++ frontend/src/app/troubleshooting/edit/[id]/page.tsx
@@ -1,29 +1,46 @@
-"use client"
+"use client";
 
-import { fetchTroubleshootingPost } from '@/libs/api/troubleshooting';
-import { Troubleshooting } from '@/types/types';
-import React, { useEffect, useState } from 'react'
+import {
+  deletePost,
+  fetchTroubleshootingPost,
+} from "@/libs/api/troubleshooting";
+import { Troubleshooting } from "@/types/types";
+import React, { useEffect, useState } from "react";
 
-const page = ({params} : {params: {id:string}}) => {
-    const [post, setPost] = useState<Troubleshooting | undefined>(undefined);
-    const id = parseInt(params.id, 10);
-    
-    useEffect(() => {
-        const fetchPost = async() => {
-            try {
-                const data = await fetchTroubleshootingPost(id);
-                setPost(data);
-            } catch (error) {
-                console.error("Error fetching troubleshooting post:", error);
-            }
-        }
-        fetchPost();
-    }, [id])
+const page = ({ params }: { params: { id: string } }) => {
+  const [post, setPost] = useState<Troubleshooting | null>(null);
+  const [title, setTitle] = useState("");
+
+  const id = parseInt(params.id, 10);
+
+  const handleDelete = async () => {
+    await deletePost(id);
+  };
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const data = await fetchTroubleshootingPost(id);
+        setPost(data);
+        setTitle(data.title);
+      } catch (error) {
+        console.error("Error fetching troubleshooting post:", error);
+      }
+    };
+    fetchPost();
+  }, [id]);
+
   return (
     <div>
-        {post ? <p>{post.title}</p> : <p>Loading...</p>}
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button
+        onClick={handleDelete}
+        className="bg-red-500 text-white px-2 py-1 rounded-md"
+      >
+        Delete
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
