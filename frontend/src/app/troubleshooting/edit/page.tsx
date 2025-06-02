df3ae98c4e0cccc180d5ffd9222fef47cba7215f
Raina Moon <mds6425@gmail.com>
2025-06-02 14:20:20 +0900

    🚚 move: move page component to render TroubleshootingSection

---
+++ frontend/src/app/troubleshooting/edit/page.tsx
@@ -0,0 +1,12 @@
+import TroubleshootingSection from '@/components/Troubleshooting/TroubleshootingSection'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <TroubleshootingSection/>
+    </div>
+  )
+}
+
+export default page
