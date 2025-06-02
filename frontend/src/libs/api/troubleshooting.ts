80eb4874fa22b698d8ac22a1c748e6c349f87a51
Raina Moon <mds6425@gmail.com>
2025-06-02 15:47:38 +0900

    ✨ feat: update API endpoints to use environment variable for troubleshooting posts

--- frontend/src/libs/api/troubleshooting.ts
+++ frontend/src/libs/api/troubleshooting.ts
@@ -1,36 +1,89 @@
 import { Troubleshooting } from "../types";
 
-export const fetchTroubleshootingPosts = async (): Promise<Troubleshooting[]> => {
-    const response = await fetch('/api/troubleshooting');
-    if (!response.ok) {
-      throw new Error('Failed to fetch troubleshooting posts');
-    }
-    const data: Troubleshooting[] = await response.json();
-    return data;
-  };
-
-  export const fetchTroubleshootingPost = async (id:number): Promise<Troubleshooting> => {
-    const response = await fetch(`/api/troubleshooting/${id}`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch troubleshooting post');
-    }
-    const data: Troubleshooting = await response.json();
-    return data;
+export const fetchTroubleshootingPosts = async (): Promise<
+  Troubleshooting[]
+> => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/troubleshooting`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch troubleshooting posts");
   }
+  const data: Troubleshooting[] = await response.json();
+  return data;
+};
 
-  export const createTroubleshootingPost = async (title:string,content: string): Promise<Troubleshooting> => {
-    const response = await fetch('/api/troubleshooting', {
-      method: 'POST',
+export const fetchTroubleshootingPost = async (
+  id: number
+): Promise<Troubleshooting> => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/troubleshooting/${id}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch troubleshooting post");
+  }
+  const data: Troubleshooting = await response.json();
+  return data;
+};
+
+export const createTroubleshootingPost = async (
+  title: string,
+  content: string,
+  tags: []
+): Promise<Troubleshooting> => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/troubleshooting`,
+    {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ title,content }),
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to create troubleshooting post');
+      body: JSON.stringify({ title, content, tags }),
     }
+  );
 
-    const data: Troubleshooting = await response.json();
-    return data;
+  if (!response.ok) {
+    throw new Error("Failed to create troubleshooting post");
   }
+
+  const data: Troubleshooting = await response.json();
+  return data;
+};
+
+export const updatePost = async (
+  id: number,
+  title: string,
+  content: string,
+  tags: []
+) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/troubleshooting/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, content, tags }),
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Failed to update troubleshooting post");
+  }
+  const data: Troubleshooting = await response.json();
+  return data;
+};
+
+export const deletePost = async (id: number) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/troubleshooting/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to delete troubleshooting post");
+  }
+
+  return true;
+};
